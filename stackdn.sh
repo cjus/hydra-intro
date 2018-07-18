9662849888980641ac6958224b092874f8195ebf
cjus <Carlos.Justiniano@gmail.com>
2018-07-18 08:19:44 -0400

    initial commit

---
+++ stackdn.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export STACK_NAME='hydra-intro'
+docker stack rm ${STACK_NAME}
---
+++ stackup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export STACK_NAME='hydra-intro'
+
+mkdir -p ~/data/${STACK_NAME}/redis
+
+echo "\nStarting core services"
+sudo STACK_NAME=${STACK_NAME} docker stack deploy --compose-file compose.yml ${STACK_NAME}
+
